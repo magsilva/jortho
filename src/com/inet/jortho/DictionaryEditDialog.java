12707e5658efadc3a569e9b6a6521abc941669b8
i_net_software <i_net_software@f9ef4159-7140-0410-9a8f-fac0b37dfe06>
2008-01-21 11:00:25 +0000

    Dialogs can be close with Escape
    git-svn-id: https://jortho.svn.sourceforge.net/svnroot/jortho/trunk/JOrtho@74 f9ef4159-7140-0410-9a8f-fac0b37dfe06

--- src/com/inet/jortho/DictionaryEditDialog.java
+++ src/com/inet/jortho/DictionaryEditDialog.java
@@ -28,6 +28,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
@@ -59,6 +60,15 @@ class DictionaryEditDialog extends JDialog implements ActionListener{
         delete = new JButton( Utils.getResource("delete") );
         content.add( delete, new GridBagConstraints( 1, 2, 1, 1, 1.0, 0.0, GridBagConstraints.NORTH ,GridBagConstraints.BOTH, new Insets( 0,8,8,8 ), 0, 0) );
         delete.addActionListener( this );
+        
+        //ESCAPE Taste
+        getRootPane().getInputMap( JComponent.WHEN_IN_FOCUSED_WINDOW ).put( KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0, false ), "ESCAPE" );
+        getRootPane().getActionMap().put( "ESCAPE", new AbstractAction() {
+            public void actionPerformed( ActionEvent e ) {
+                dispose();
+            }
+        } );
+
         pack();
         setLocationRelativeTo( parent );
     }
--- src/com/inet/jortho/SpellCheckerDialog.java
+++ src/com/inet/jortho/SpellCheckerDialog.java
@@ -25,6 +25,7 @@ package com.inet.jortho;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 
@@ -126,6 +127,14 @@ class SpellCheckerDialog extends JDialog implements ActionListener {
         change.addActionListener(this);
         changeAll.addActionListener(this);
         finish.addActionListener(this);
+        
+        //ESCAPE Taste
+        finish.getInputMap( JComponent.WHEN_IN_FOCUSED_WINDOW ).put( KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0, false ), "ESCAPE" );
+        finish.getActionMap().put( "ESCAPE", new AbstractAction() {
+            public void actionPerformed( ActionEvent e ) {
+                dispose();
+            }
+        } );
         
         boolean isUserDictionary = SpellChecker.getUserDictionaryProvider() != null;
         addToDic.setEnabled( isUserDictionary );
