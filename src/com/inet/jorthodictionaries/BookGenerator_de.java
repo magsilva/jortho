900393cb11a9e26311be1a561e875151c791a9a0
i_net_software <i_net_software@f9ef4159-7140-0410-9a8f-fac0b37dfe06>
2010-07-09 13:58:19 +0000

    parse also the additional konjugation tables of verbs in the German dictionary
    git-svn-id: https://jortho.svn.sourceforge.net/svnroot/jortho/trunk/JOrtho@191 f9ef4159-7140-0410-9a8f-fac0b37dfe06

--- src/com/inet/jorthodictionaries/BookGenerator_de.java
+++ src/com/inet/jorthodictionaries/BookGenerator_de.java
@@ -32,20 +32,25 @@ import java.util.regex.Pattern;
  */
 public class BookGenerator_de extends BookGenerator {
 
+    public BookGenerator_de(){
+        addWord( "Ihnen" );
+        addWord( "Sie" );
+        addWord( "Du" );
+    }
+    
     @Override
     protected boolean isValidWord(String word){
         if(super.isValidWord( word )){
             return true;
         }
-        return word.endsWith( "(Deklination)" );
+        return word.endsWith( "(Deklination)" ) || word.endsWith( "(Konjugation)" );
     }
     
     @Override
-    boolean isValidLanguage(String word, String wikiText) {       
+    boolean isValidLanguage(String word, String wikiText) {   
         wikiText = removeHtmlFormating(wikiText);
         int idxGerman = wikiText.indexOf("{{Sprache|Deutsch}}");
-        if( idxGerman < 0){
-            
+        if( idxGerman < 0 || word.endsWith( "(Deklination)" ) || word.endsWith( "(Konjugation)" )){
             /*
             if(wikiText.indexOf("{{Sprache|") >= 0){
                 return false;
@@ -68,6 +73,59 @@ public class BookGenerator_de extends BookGenerator {
                 // The declinations are valid, but the lemma (word) not.
                 return false;
             }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb unregelmäßig", 0 );
+            if(props != null){
+                addKonjugationVerbUnregular( props );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb schwach untrennbar", 0 );
+            if(props != null){
+                addKonjugationVerbSchwachUntrennbar( props, false );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb schwach untrennbar reflexiv", 0 );
+            if(props != null){
+                addKonjugationVerbSchwachUntrennbar( props, true );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb schwach trennbar", 0 );
+            if(props != null){
+                addKonjugationVerbSchwachTrennbar( props, false );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb schwach trennbar reflexiv", 0 );
+            if(props != null){
+                addKonjugationVerbSchwachTrennbar( props, true );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Deutsch Verb schwach doppelt trennbar", 0 );
+            if(props != null){
+                addKonjugationVerbSchwachDoppeltTrennbar( props );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+            
+            props = BookUtils.parseRule( wikiText, "Konjugation Hilfsverb haben", 0 );
+            if(props != null){
+                addKonjugationHilfsverbHaben( props );
+                // The declinations are valid, but the lemma (word) not.
+                return false;
+            }
+
+//            if( word.endsWith( "(Konjugation)" ) && wikiText.indexOf( "Deutsch" ) > 0 ){
+//                System.err.println();
+//            }
             
             return false;
         }
@@ -377,7 +435,7 @@ public class BookGenerator_de extends BookGenerator {
      * @param wordStamm the word root, can be null
      */
     private void addDeklinationAdjektiv( String wordStamm ) {
-        if( wordStamm != null && wordStamm.length() > 1 ) {
+        if( wordStamm != null && isValidWord( wordStamm )) {
             addWord( wordStamm + "e" ); 
             addWord( wordStamm + "er" );
             addWord( wordStamm + "es" );
@@ -480,7 +538,7 @@ public class BookGenerator_de extends BookGenerator {
         String plural = props.getProperty( "PLURAL", "" );
         String genetiv = props.getProperty( "GENITIV-E", "" );
         String endung = props.getProperty( "ENDUNGS-N", "" );
-        if( singular.length() > 0 ) {
+        if( super.isValidWord( singular ) ) {
             if( genetiv.length() == 0 || "0".equals( genetiv ) ) {
                 addWord( singular + "s" ); //Genitive
             }
@@ -489,7 +547,7 @@ public class BookGenerator_de extends BookGenerator {
                 addWord( singular + "e" ); //Dative
             }
         }
-        if( plural.length() > 0 ) {
+        if( super.isValidWord( plural ) ) {
             addWord( plural );
             if( endung.length() == 0 || "0".equals( endung ) ) {
                 addWord( plural + "n" );
@@ -509,12 +567,12 @@ public class BookGenerator_de extends BookGenerator {
         String singular = props.getProperty( "SINGULAR", "" );
         String plural = props.getProperty( "PLURAL", "" );
         String genetiv = props.getProperty( "GENITIV-E", "" );
-        if( singular.length() > 0 ) {
+        if( super.isValidWord( singular ) ) {
             if( "1".equals( genetiv ) ) {
                 addWord( singular + "n" );
             }
         }
-        if( plural.length() > 0 ) {
+        if( super.isValidWord( plural ) ) {
             addWord( plural );
         }
         return true;
@@ -531,14 +589,14 @@ public class BookGenerator_de extends BookGenerator {
         String singular = props.getProperty( "SINGULAR", "" );
         String plural = props.getProperty( "PLURAL", "" );
         String genetiv = props.getProperty( "GENITIV-E", "" );
-        if( singular.length() > 0 ) {
+        if( super.isValidWord( singular ) ) {
             if( "1".equals( genetiv ) ) {
                 addWord( singular + "es" );
             } else {
                 addWord( singular + "s" );
             }
         }
-        if( plural.length() > 0 ) {
+        if( super.isValidWord( plural ) ) {
             addWord( plural );
         }
         return true;
@@ -555,7 +613,7 @@ public class BookGenerator_de extends BookGenerator {
         //String singular = props.getProperty( "SINGULAR", "" );
         String plural = props.getProperty( "PLURAL", "" );
         String pluralN = props.getProperty( "PLURAL AUF N?", "" );
-        if( plural.length() > 0 ) {
+        if( super.isValidWord( plural ) ) {
             addWord( plural );
             if( !"ja".equals( pluralN ) ) {
                 addWord( plural + "n" );
@@ -564,4 +622,213 @@ public class BookGenerator_de extends BookGenerator {
         return true;
     }
 
+    /**
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_unregelm%C3%A4%C3%9Fig
+     */
+    private void addKonjugationVerbUnregular( Properties props ) {
+        String stamm = props.getProperty( "2" );
+        // Partizipien
+        addWord( stamm + "end" );
+        
+        //Präsens
+        if( stamm != null && super.isValidWord( stamm ) ) {
+            addWord( stamm + "e" );
+            addWord( stamm + "st" );
+            addWord( stamm + "t" );
+            addWord( stamm + "en" );
+            addWord( stamm + "est" );
+            addWord( stamm + "et" );
+        }
+
+        //Präteritum
+        stamm = props.getProperty( "3" );
+        if( stamm != null && super.isValidWord( stamm ) ) {
+            addWord( stamm );
+            addWord( stamm + "st" );
+            addWord( stamm + "en" );
+            addWord( stamm + "t" );
+        }
+
+        //Präteritum Konjunktiv
+        stamm = props.getProperty( "4" );
+        if( stamm != null && super.isValidWord( stamm ) ) {
+            addWord( stamm + "e" );
+            addWord( stamm + "est" );
+            addWord( stamm + "en" );
+            addWord( stamm + "et" );
+        }
+
+        stamm = props.getProperty( "5" );
+        if( stamm != null && super.isValidWord( stamm ) ) {
+            addWord( stamm );
+        }
+    }
+    
+    /**
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_schwach_untrennbar
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_schwach_untrennbar_reflexiv
+     */
+    private void addKonjugationVerbSchwachUntrennbar( Properties props, boolean reflexiv ) {
+        String partizip2 = props.getProperty( "6" );
+        if( partizip2 != null && super.isValidWord( partizip2 ) ) {
+            addWord( partizip2 );
+        }
+
+        String stamm4 = props.getProperty( "1" ) + props.getProperty( "2" ) + props.getProperty( "3" ) + props.getProperty( "4" );
+        if( super.isValidWord( stamm4 ) ) {
+            // Partizipien
+            addWord( stamm4 + "nd" );
+            if( !reflexiv ){
+                addWord( stamm4 + "nder" );
+                addWord( stamm4 + "nde" );
+                addWord( stamm4 + "ndes" );
+                addWord( stamm4 + "nden" );
+                addWord( stamm4 + "ndem" );
+            }
+            
+            String stamm3 = props.getProperty( "1" ) + props.getProperty( "2" ) + props.getProperty( "3" );
+            //Präsens
+            addWord( stamm4 );
+            addWord( stamm3 + "st" );
+            addWord( stamm3 + "t" );
+            addWord( stamm3 + "est" );
+            addWord( stamm3 + "e" );
+            addWord( stamm3 + "et" );
+            //Präterium
+            addWord( stamm3 + "te" );
+            addWord( stamm3 + "test" );
+            addWord( stamm3 + "ten" );
+            addWord( stamm3 + "tet" );
+        }
+    }
+    
+    /**
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_schwach_trennbar
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_schwach_trennbar_reflexiv
+     */
+    private void addKonjugationVerbSchwachTrennbar( Properties props, boolean reflexiv ) {
+        String partizip2 = props.getProperty( "7" );
+        if( partizip2 != null && super.isValidWord( partizip2 ) ) {
+            addWord( partizip2 );
+        }
+        
+        String vorsilbe = props.getProperty( "1" );
+        String stamm4 = props.getProperty( "2" ) + props.getProperty( "3" ) + props.getProperty( "4" );
+        String stamm5 = stamm4 + props.getProperty( "5" );
+        String all4 = vorsilbe + stamm4;
+        String all5 = vorsilbe + stamm5;
+        
+        addWord( vorsilbe + "zu" + stamm5 + "n" );
+        // Partizipien
+        addWord( all5 + "nd" );
+        if( !reflexiv ){
+            addWord( vorsilbe + "zu" + stamm5 + "nder" );
+            addWord( vorsilbe + "zu" + stamm5 + "nde" );
+            addWord( vorsilbe + "zu" + stamm5 + "ndes" );
+            addWord( vorsilbe + "zu" + stamm5 + "nden" );
+            addWord( vorsilbe + "zu" + stamm5 + "ndem" );
+        }
+        //Präsens
+        addWord( stamm5 );
+        addWord( stamm4 + "st" );
+        addWord( stamm4 + "t" );
+        addWord( stamm4 + "est" );
+        addWord( stamm4 + "e" );
+        addWord( stamm5 + "n" );
+        addWord( stamm4 + "et" );
+        addWord( all5 );
+        addWord( all4 + "st" );
+        addWord( all4 + "t" );
+        addWord( all4 + "est" );
+        addWord( all4 + "e" );
+        addWord( all5 + "n" );
+        addWord( all4 + "et" );
+        //Präterium
+        addWord( stamm4 + "te" );
+        addWord( stamm4 + "test" );
+        addWord( stamm4 + "ten" );
+        addWord( stamm4 + "tet" );
+        addWord( all4 + "te" );
+        addWord( all4 + "test" );
+        addWord( all4 + "ten" );
+        addWord( all4 + "tet" );
+    }
+    
+    /**
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Deutsch_Verb_schwach_doppelt_trennbar
+     */
+    private void addKonjugationVerbSchwachDoppeltTrennbar( Properties props ) {
+        String partizip2 = props.getProperty( "8" );
+        if( partizip2 != null && super.isValidWord( partizip2 ) ) {
+            addWord( partizip2 );
+        }
+        
+        String vorsilbe = props.getProperty( "1" ) + props.getProperty( "2" );;
+        String stamm5 = props.getProperty( "3" ) + props.getProperty( "4" ) + props.getProperty( "5" );
+        String stamm6 = stamm5 + props.getProperty( "6" );
+        String all5 = vorsilbe + stamm5;
+        String all6 = vorsilbe + stamm6;
+        
+        addWord( vorsilbe + "zu" + stamm6 + "n" );
+        // Partizipien
+        addWord( all6 + "nd" );
+        addWord( vorsilbe + "zu" + stamm6 + "nder" );
+        addWord( vorsilbe + "zu" + stamm6 + "nde" );
+        addWord( vorsilbe + "zu" + stamm6 + "ndes" );
+        addWord( vorsilbe + "zu" + stamm6 + "nden" );
+        addWord( vorsilbe + "zu" + stamm6 + "ndem" );
+        //Präsens
+        addWord( stamm6 );
+        addWord( stamm5 + "st" );
+        addWord( stamm5 + "t" );
+        addWord( stamm5 + "est" );
+        addWord( stamm5 + "e" );
+        addWord( stamm6 + "n" );
+        addWord( stamm5 + "et" );
+        addWord( all6 );
+        addWord( all5 + "st" );
+        addWord( all5 + "t" );
+        addWord( all5 + "est" );
+        addWord( all5 + "e" );
+        addWord( all6 + "n" );
+        addWord( all5 + "et" );
+        //Präterium
+        addWord( stamm5 + "te" );
+        addWord( stamm5 + "test" );
+        addWord( stamm5 + "ten" );
+        addWord( stamm5 + "tet" );
+        addWord( all5 + "te" );
+        addWord( all5 + "test" );
+        addWord( all5 + "ten" );
+        addWord( all5 + "tet" );
+    }
+    
+    /**
+     * Implementation of the template http://de.wiktionary.org/wiki/Vorlage:Konjugation_Hilfsverb_haben
+     */
+    private void addKonjugationHilfsverbHaben( Properties props ) {
+        String partizip2 = props.getProperty( "3" );
+        if( partizip2 != null && super.isValidWord( partizip2 ) ) {
+            addWord( partizip2 );
+        }
+        String stamm1 = props.getProperty( "1" );
+        String stamm2 = props.getProperty( "2" );
+        if( super.isValidWord( stamm1 ) && super.isValidWord( stamm2 ) ) {
+            // Partizipien
+            addWord( stamm1 + "end" );
+            //Präsens
+            addWord( stamm1 + "e" );
+            addWord( stamm2 + "st" );
+            addWord( stamm2 + "t" );
+            //Präterium
+            addWord( stamm2 + "te" );
+            addWord( stamm2 + "test" );
+            addWord( stamm2 + "ten" );
+            addWord( stamm2 + "tet" );
+            //Konjunktiv I
+            //addWord( stamm1 + "e" );
+            addWord( stamm1 + "est" );
+            addWord( stamm2 + "et" );
+        }
+    }
 }
